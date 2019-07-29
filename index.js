88833c8f40de0027d8f15fabfc35e27bbda3936e
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-07-28 23:09:01 -0300

    First commit - usuario and product types created

---
+++ index.js
@@ -0,0 +1,76 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    scalar Date
+
+    type Usuario {
+        id: ID
+        nome: String!
+        email: String!
+        idade: Int
+        salario: Float
+        vip: Boolean
+    }
+
+    type Produto {
+        nome: String!
+        preco: Float!
+        desconto: Float
+        precoComDesconto: Float
+    }
+
+    # Pontos de entrada da sua API
+    type Query {
+        ola: String!
+        horaCerta: Date!
+        usuarioLogado: Usuario
+        produtoEmDestaque: Produto
+    }    
+`
+
+const resolvers = {
+    Usuario: {
+        salario(usuario) {
+            return usuario.salario_real;
+        }
+    },
+    Produto: {
+        precoComDesconto(produto) {
+            return produto.preco - (produto.preco * produto.desconto);
+        }
+    },
+    Query: {
+        ola() {
+            return 'Basta retornar uma string'
+        },
+        horaCerta() {
+            return new Date
+        },
+        usuarioLogado() {
+            return {
+                id: 1,
+                nome: 'Teste da Silva',
+                email: 'teste@teste.com',
+                idade: 30,
+                salario_real: 2000.50,
+                vip: true
+            }
+        },
+        produtoEmDestaque() {
+            return {
+                nome: 'Produto Teste',
+                preco: 20.00,
+                desconto: 0.1
+            }
+        }
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
