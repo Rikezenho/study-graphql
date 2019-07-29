118e4b6e3d5bc4ee801f6372184a349b388a2cd6
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-07-29 00:25:56 -0300

    Removing unused function gql

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer } = require('apollo-server')
 const { importSchema } = require('graphql-import')
 const resolvers = require('./resolvers')
 
