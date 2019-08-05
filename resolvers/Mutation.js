29a10c9e868c57a44863ef9535573467cf70b178
Henrique Kuwai <eu@henriquekuwai.com.br>
2019-08-05 00:37:54 -0300

    adds first validation in mutation

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -2,6 +2,11 @@ const { usuarios, proximoId }  = require('../data/db')
 
 module.exports = {
     novoUsuario(_, { nome, email, idade }) {
+        const emailExiste = usuarios
+            .some(u => u.email === email)
+        
+        if (emailExiste) throw new Error('E-mail já cadastrado!')
+        
         const novo = {
             id: proximoId(),
             nome,
